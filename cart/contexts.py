36a03b6b23cabdd1321d828224b3d99693bffc00
ChristianPlesca <cristiplesca1993@gmail.com>
2020-06-10 17:53:24 +0100

    Added notifications CSS, shoping cart preview and additional messages

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,43 +9,46 @@ def cart_contents(request):
     total = 0
     product_count = 0
     cart = request.session.get('cart', {})
-    
+
     for item_id, item_data in cart.items():
-        if isinstance(item_data,int):
+        if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
             cart_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
-                'product':product
+                'product': product,
             })
         else:
+            product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                product = get_object_or_404(Product, pk=item_id)
                 total += quantity * product.price
                 product_count += quantity
                 cart_items.append({
-                'item_id': item_id,
-                'quantity': quantity,
-                'product': product,
-                'size':size
-            })               
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.STANDARD_DELIVERY_PERCENTAGE - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-    contexts = {'cart_items':cart_items,
-                'total':total,
-                'product_count':product_count,
-                'delivery':delivery,
-                'free_delivery_delta':free_delivery_delta,
-                'free_delivery_threshold':settings.FREE_DELIVERY_THRESHOLD,
-                'grand_total': grand_total,
-                }
-    return contexts
+    
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
