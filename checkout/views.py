d73a7732b2efd0b41d68843f87d3f763ef7b7942
Cristian Plesca <cristiplesca1993@gmail.com>
2020-06-12 01:51:32 +0100

    Update views.py

--- checkout/views.py
+++ checkout/views.py
@@ -14,8 +14,6 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_q4o8w3CrmWjVoEaNg2sCMxx900pWCBqSLs',
-        'client_secret':'sk_test_49jlYJQZ6hND57wBTdtcqRG900jvacvwgU'
     }
 
     return render(request, template, context)
