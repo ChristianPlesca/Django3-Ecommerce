c36cc1cbfd81e5ce569e762e2f691f2fa4161ab3
ChristianPlesca <cristiplesca1993@gmail.com>
2020-06-16 16:45:31 +0100

    add product-admin view,templates and url

--- products/forms.py
+++ products/forms.py
@@ -11,6 +11,7 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
+        #The syntax below is a list comprehansion
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
